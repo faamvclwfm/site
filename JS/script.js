7d60ab5140c3bdeb014df59f28a184badb2bd84e
ArsenFedchyk <151182636+faamvclwfm@users.noreply.github.com>
2024-10-10 21:02:09 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+
+let button1 = document.getElementById('6')
+
+button1.addEventListener('click', () => {
+    const cont1=document.getElementById('71')
+    cont1.style.display='block'
+});
+let button2 = document.getElementById('71')
+
+button2.addEventListener('click', () => {
+    const cont1=document.getElementById('81')
+    cont1.style.display='block'
+});
+let button3 = document.getElementById('81')
+
+button3.addEventListener('click', () => {
+    const cont1=document.getElementById('91')
+    cont1.style.display='block'
+});
+let button4 = document.getElementById('91')
+
+button4.addEventListener('click', () => {
+    const cont1=document.getElementById('101')
+    cont1.style.display='block'
+});
+let button5 = document.getElementById('101')
+
+button5.addEventListener('click', () => {
+    const cont1=document.getElementById('111')
+    cont1.style.display='block'
+});
+let button6 = document.getElementById('12')
+
+button6.addEventListener('click', () => {
+    const cont1=document.getElementById('71')
+    const cont2=document.getElementById('81')
+    const cont3=document.getElementById('91')
+    const cont4=document.getElementById('101')
+    const cont5=document.getElementById('111')
+    
+    
+
+    
+    cont1.style.display='none'
+    cont2.style.display='none'
+    cont3.style.display='none'
+    cont4.style.display='none'
+    cont5.style.display='none'
+
+});
