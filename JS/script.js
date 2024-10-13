2e3af98639a5cfef8d87a666a3f08bfa2f2b7470
ArsenFedchyk <arsenfedchyk123@gmail.com>
2024-10-13 19:03:23 +0300

    change button`s action

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,17 @@
 
-const button1 = document.getElementById('5')
-button1.addEventListener('click', () => {
-    const cont1=document.getElementById('6')
-    cont1.style.display='block'
-});
-const button2 = document.getElementById('7')
-
-button2.addEventListener('click', () => {
-    const cont1=document.getElementById('8')
-    cont1.style.display='block'
-});
-const button3 = document.getElementById('9')
-
-button3.addEventListener('click', () => {
-    const cont1=document.getElementById('10')
-    cont1.style.display='block'
-});
-const button4 = document.getElementById('11')
-
-button4.addEventListener('click', () => {
-    const cont1=document.getElementById('12')
-    cont1.style.display='block'
-});
-const button5 = document.getElementById('13')
-
-button5.addEventListener('click', () => {
-    const cont1=document.getElementById('14')
-    cont1.style.display='block'
-});
+for (let i=5;i<14;i++) {
+    const buttonID =document.getElementById(i)
+    buttonID.addEventListener('click', () => {
+        const containerId =document.getElementById(i+1)
+        containerId.style.display='block'
+    })
+}
 const button6 = document.getElementById('15')
-
 button6.addEventListener('click', () => {
-    
     for (let i=6;i<15;i) {
         
         let containerNumber =document.getElementById(i)
         containerNumber.style.display='none'
         i=i+2
     }
-    
-
 });
